30522311be8f60c0751c0d27f2d3d3c5b1c35dcf
voivashchenko <60275399+voivashchenko@users.noreply.github.com>
2020-01-28 01:08:35 +0300

    Add files via upload

---
+++ Home Work.py
@@ -0,0 +1,16 @@
+q, w, e, r = 1, 2, 3, 4
+print(q + w);
+print(w - e);
+print(e * r);
+print(r / q)
+
+a = 'hello'
+s = ' world'
+print(a + s)
+
+
+import random
+k = [random.randint(1, 50)
+for i in range(10)]
+del k [-1]
+print(k)
